7373946475b1d5fa64d54cc5bb5678f564a54541
Adrian Looney <adrian.looney@gmail.com>
2021-02-21 13:38:56 +0000

    Set up deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn darkcastlebrewery.wsgi:application
--- dark_castle/settings.py
+++ dark_castle/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'igo8(u4oyyr^%beop)zx2u3#dv97)p@d8t+y!xor)ws@63$$g&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['darkcastlebrewery.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -115,12 +116,18 @@ WSGI_APPLICATION = 'dark_castle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
