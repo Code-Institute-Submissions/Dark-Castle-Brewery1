1ac565e4e70bd19b24a7729051ddf80e2f75c766
Adrian Looney <adrian.looney@gmail.com>
2021-02-22 15:55:44 +0000

    email sending error

--- dark_castle/settings.py
+++ dark_castle/settings.py
@@ -210,9 +210,9 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-if 'DEVELOPMENT' in os.environ:
+if 'DEVELOMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'darkcastlebrewery@example.com'
+    DEFAULT_FROM_EMAIL ='boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -220,6 +220,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-
+    DEFAULT_FROM_EMAIL  = os.environ.get('EMAIL_HOST_USER')
